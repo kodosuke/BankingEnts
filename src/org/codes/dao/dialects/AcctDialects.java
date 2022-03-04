3172e8c0b6947a01cb5e3c3e893e06c745cab6dd
kodosuke <ajaykrishnaanandhan@yahoo.com>
2022-03-04 20:39:10 +0530

    Create AcctDialects

---
+++ src/org/codes/dao/dialects/AcctDialects.java
@@ -0,0 +1,11 @@
+package org.codes.dao.dialects;
+
+public interface AcctDialects {
+    String ALL_ACCOUNTS = "SELECT * FROM accounts";
+    String ACCOUNT_BY_NUMBER = "SELECT * FROM accounts WHERE accountNumber = ?";
+    String UPDATE_BALANCE = "UPDATE accounts SET balance = ? WHERE accountNumber = ?";
+    String UPDATE_PASSWORD = "UPDATE accounts SET password = ? WHERE accountNumber = ?";
+    String RETRIEVE_PASSWORD = "SELECT password FROM accounts WHERE accountNumber = ?";
+    String INSERT_ACCOUNT = "INSERT INTO accounts(accountNumber, customerName, balance, password, createTime) VALUES(?, ?, ?, ?, ?)";
+
+}
